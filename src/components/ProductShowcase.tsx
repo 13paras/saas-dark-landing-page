6ec450b0d32788e88ba575e932cb49b96857852d
Paras <sakarwalparas@gmail.com>
2024-08-06 17:25:19 +0530

    productShowcase animation

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -1,7 +1,19 @@
+"use client";
+
 import Image from "next/image";
 import appScreen from "../assets/images/app-screen.png";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useEffect, useRef } from "react";
 
 export const ProductShowcase = () => {
+  const appImage = useRef<HTMLImageElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: appImage,
+    offset: ["start end", "end end"],
+  });
+
+  const rotateX = useTransform(scrollYProgress, [0, 1], [15, 0]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [.5, 1]);
   return (
     <div className="bg-black bg-gradient-to-b from-black to-[#3D2CA8] py-[72px] text-white sm:py-24">
       <div className="container">
@@ -15,7 +27,20 @@ export const ProductShowcase = () => {
             one task at a time.
           </p>
         </div>
-        <Image src={appScreen} alt="The product screenshot" className="mt-14 lg:w-[70%] mx-auto" />
+        <motion.div
+          style={{
+            opacity: opacity,
+            rotateX: rotateX,
+            transformPerspective: "800px",
+          }}
+        >
+          <Image
+            src={appScreen}
+            alt="The product screenshot"
+            className="mx-auto mt-14 lg:w-[70%]"
+            ref={appImage}
+          />
+        </motion.div>
       </div>
     </div>
   );
