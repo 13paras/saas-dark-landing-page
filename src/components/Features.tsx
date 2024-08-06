0dd920296d806cf7e8c919287737daf38ef9eaaa
Paras <sakarwalparas@gmail.com>
2024-08-06 13:03:20 +0530

    added: features section

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,3 +1,5 @@
+import EcosystemIcon from "../assets/icons/ecosystem.svg";
+
 const features = [
   {
     title: "Integration Ecosystem",
@@ -17,5 +19,34 @@ const features = [
 ];
 
 export const Features = () => {
-  return null;
+  return (
+    <div className="bg-black py-[72px] text-white sm:py-24">
+      <div className="container">
+        <h2 className="text-center text-5xl font-bold tracking-tighter sm:text-6xl">
+          Everything you need
+        </h2>
+        <div className="mx-auto max-w-xl">
+          <p className="mt-5 text-center text-xl text-white/70">
+            Enjoy customizable lists, team work tools, and smart tracking all in
+            one place. Set tasks, get reminders, and see your progress simply
+            and quickly.
+          </p>
+        </div>
+        <div className="mt-16 flex flex-col gap-4 sm:flex-row">
+          {features.map(({ title, description }) => (
+            <div
+              key={title}
+              className="rounded-xl border border-white/30 px-5 py-10 text-center sm:flex-1"
+            >
+              <div className="inline-flex h-14 w-14 items-center justify-center rounded-lg bg-white text-black">
+                <EcosystemIcon />
+              </div>
+              <h3 className="mt-6 font-bold"> {title} </h3>
+              <p className="mt-2 text-white/70"> {description} </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
