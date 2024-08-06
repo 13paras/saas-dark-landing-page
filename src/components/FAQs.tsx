4266c056e80de409d51d0ea32cbbc1bdddda3f74
Paras <sakarwalparas@gmail.com>
2024-08-06 17:29:37 +0530

    animated accordion

--- src/components/FAQs.tsx
+++ src/components/FAQs.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import clsx from "clsx";
 import PlusIcon from "../assets/icons/plus.svg";
 import MinusIcon from "../assets/icons/minus.svg";
+import { AnimatePresence, motion } from "framer-motion";
 
 const items = [
   {
@@ -46,14 +47,31 @@ const AccordionItem = ({
         <span className="flex-1 text-lg font-bold">{question}</span>
         {isOpen ? <MinusIcon /> : <PlusIcon />}
       </div>
-      <div
-        className={clsx("mt-4", {
-          hidden: !isOpen,
-          "": isOpen === true,
-        })}
-      >
-        {answer}
-      </div>
+
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+           
+            initial={{
+              opacity: 0,
+              height: 0,
+              marginTop: 0
+            }}
+            animate={{
+              opacity: 1,
+              height: "auto",
+              marginTop: 16
+            }}
+            exit={{
+              opacity: 0,
+              height: 0,
+              marginTop: 0
+            }}
+          >
+            {answer}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
